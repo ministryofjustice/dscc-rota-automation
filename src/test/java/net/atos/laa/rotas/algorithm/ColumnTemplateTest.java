642a5bdca2317d6f3a2f73d4a7c7ece56d85f782
Bethan Palmer <bethan.palmer@digital.justice.gov.uk>
2020-09-18 17:34:07 +0100

    Add unit tests for adding and removing individual dates to the rota

--- src/test/java/net/atos/laa/rotas/algorithm/ColumnTemplateTest.java
+++ src/test/java/net/atos/laa/rotas/algorithm/ColumnTemplateTest.java
@@ -2,6 +2,7 @@ package net.atos.laa.rotas.algorithm;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import net.atos.laa.rotas.algorithm.dataset.Scheme;
 import net.atos.laa.rotas.algorithm.rota.ColumnType;
@@ -51,6 +52,34 @@ class ColumnTemplateTest {
     columnTemplate = new ColumnTemplate(columnType, title, schemes, null, blanksPatternModifiers);
     String modifiers = columnTemplate.getChosenModifiers();
     assertEquals("NP", modifiers);
+  }
+
+  @Test
+  public void testAddingAnIndividualDateToTheRota() {
+    ArrayList<BlanksModifier> blanksPatternModifiers = new ArrayList<>();
+    LocalDate localDate = LocalDate.now();
+    BlanksModifier blanksModifier = new BlanksModifier(BlanksModifierType.ADD, localDate);
+    blanksPatternModifiers.add(blanksModifier);
+    columnTemplate = new ColumnTemplate(columnType, title, schemes, null, blanksPatternModifiers);
+    columnTemplate.setBlankSlots(new ArrayList<>());
+    columnTemplate.addOrRemoveIndividualDates();
+    assertEquals(LocalDate.now(), columnTemplate.getBlankSlots().get(0));
+  }
+
+  @Test
+  public void testRemovingAnIndividualDateFromTheRota() {
+    ArrayList<BlanksModifier> blanksPatternModifiers = new ArrayList<>();
+    ArrayList<LocalDate> blankSlots = new ArrayList<>();
+    LocalDate dayOne = LocalDate.of(2020, 1, 1);
+    LocalDate dayTwo = LocalDate.of(2020, 1, 2);
+    blankSlots.add(dayOne);
+    blankSlots.add(dayTwo);
+    BlanksModifier blanksModifier = new BlanksModifier(BlanksModifierType.REMOVE, dayOne);
+    blanksPatternModifiers.add(blanksModifier);
+    columnTemplate = new ColumnTemplate(columnType, title, schemes, null, blanksPatternModifiers);
+    columnTemplate.setBlankSlots(blankSlots);
+    columnTemplate.addOrRemoveIndividualDates();
+    assertEquals(1, columnTemplate.getBlankSlots().size());
   }
 
 
